cba1aec7fc9785e59b38ebdfa39f501f88dce1fb
Trevor Keller <trevor.keller@gmail.com>
2017-08-12 19:52:43 -0400

    add OpenMP CPU diffusion code
    Fixes #16

---
+++ cpu/openmp/boundaries.c
@@ -0,0 +1,81 @@
+/*
+	File: boundaries.c
+	Role: implementation of boundary condition functions
+
+	Questions/comments to trevor.keller@nist.gov
+	Bugs/requests to https://github.com/tkphd/accelerator-testing
+*/
+
+#include <math.h>
+#include <omp.h>
+
+#include "diffusion.h"
+
+void set_boundaries(double bc[2][2])
+{
+	/* indexing is A[y][x], so bc = [[ylo,yhi], [xlo,xhi]] */
+	double clo = 0.0, chi = 1.0;
+	bc[0][0] = clo; /* bottom boundary */
+	bc[0][1] = clo; /* top boundary */
+	bc[1][0] = chi; /* left boundary */
+	bc[1][1] = chi; /* right boundary */
+}
+
+void apply_initial_conditions(double** A, int nx, int ny, double bc[2][2])
+{
+	#pragma omp parallel
+	{
+		int i, j;
+
+		#pragma omp for collapse(2)
+		for (j = 1; j < ny; j++)
+			for (i = 1; i < nx; i++)
+				A[j][i] = bc[0][0];
+
+		#pragma omp for nowait
+		for (j = 1; j < ny/2; j++)
+			A[j][1] = bc[1][0]; /* left half-wall */
+
+		#pragma omp for
+		for (j = ny/2; j < ny-1; j++)
+			A[j][nx-1] = bc[1][1]; /* right half-wall */
+	}
+}
+
+void apply_boundary_conditions(double** A, int nx, int ny, double bc[2][2])
+{
+	#pragma omp parallel
+	{
+		/* Set fixed value (c=1) along left and bottom, zero-flux elsewhere */
+		int i, j;
+
+		/* left boundary */
+		#pragma omp for
+		for (j = 1; j < ny/2; j++) {
+			A[j][0] = bc[1][0]; /* fixed value */
+			A[j][1] = bc[1][0];
+		}
+		#pragma omp for nowait
+		for (j = ny/2; j < ny-1; j++)
+			A[j][0] = A[j][1]; /* no-flux */
+
+		/* right boundary */
+		#pragma omp for
+		for (j = ny/2; j < ny-1; j++) {
+			A[j][nx-1] = bc[1][1]; /* fixed value */
+			A[j][nx-2] = bc[1][1];
+		}
+		#pragma omp for nowait
+		for (j = 1; j < ny/2; j++)
+			A[j][nx-1] = A[j][nx-2]; /* no-flux */
+
+		/* bottom boundary */
+		#pragma omp for
+		for (i = 1; i < nx-1; i++)
+			A[0][i] = A[1][i]; /* no-flux */
+
+		/* top boundary */
+		for (i = 1; i < nx-1; i++)
+			A[ny-1][i] = A[ny-2][i]; /* no-flux */
+	}
+}
---
+++ cpu/openmp/discretization.c
@@ -0,0 +1,101 @@
+/*
+	File: discretization.c
+	Role: implementation of discretized mathematical operations
+
+	Questions/comments to trevor.keller@nist.gov
+	Bugs/requests to https://github.com/tkphd/accelerator-testing
+*/
+
+#include <math.h>
+#include <omp.h>
+
+#include "diffusion.h"
+
+void set_mask(double dx, double dy, int* nm, double** M)
+{
+	/* M is initialized to zero, so corners can be ignored */
+	*nm = 1;
+
+	M[0][1] =  1.0 / (dy * dy); /* up */
+	M[1][0] =  1.0 / (dx * dx); /* left */
+	M[1][1] = -2.0 * (dx*dx + dy*dy) / (dx*dx * dy*dy); /* middle */
+	M[1][2] =  1.0 / (dx * dx); /* right */
+	M[2][1] =  1.0 / (dy * dy); /* down */
+}
+
+void compute_convolution(double** A, double** C, double** M, int nx, int ny, int nm)
+{
+	#pragma omp parallel
+	{
+		int i, j, mi, mj;
+		double value;
+
+		#pragma omp for collapse(2)
+		for (j = 1; j < ny-1; j++) {
+			for (i = 1; i < nx-1; i++) {
+				value = 0.0;
+				for (mj = -nm; mj < nm+1; mj++) {
+					for (mi = -nm; mi < nm+1; mi++) {
+						value += M[mj+nm][mi+nm] * A[j+mj][i+mi];
+					}
+				}
+				C[j][i] = value;
+			}
+		}
+	}
+}
+
+void step_in_time(double** A, double** B, double** C, int nx, int ny, double D, double dt, double* elapsed)
+{
+	#pragma omp parallel
+	{
+		int i, j;
+
+		#pragma omp for collapse(2)
+		for (j = 1; j < ny-1; j++)
+			for (i = 1; i < nx-1; i++)
+				B[j][i] = A[j][i] + dt * D * C[j][i];
+	}
+
+	*elapsed += dt;
+}
+
+void analytical_value(double x, double t, double D, double bc[2][2], double* c)
+{
+	*c = bc[1][0] * (1.0 - erf(x / sqrt(4.0 * D * t)));
+}
+
+void check_solution(double** A, int nx, int ny, double dx, double dy, double elapsed, double D, double bc[2][2], double* rss)
+{
+	double sum=0.;
+	#pragma omp parallel reduction(+:sum)
+	{
+		int i, j;
+		double r, cal, car, ca, cn, trss;
+
+		#pragma omp for collapse(2)
+		for (j = 1; j < ny-1; j++) {
+			for (i = 1; i < nx-1; i++) {
+				/* numerical solution */
+				cn = A[j][i];
+
+				/* shortest distance to left-wall source */
+				r = (j < ny/2) ? dx * (i - 1) : sqrt(dx*dx * (i - 1) * (i - 1) + dy*dy * (j - ny/2) * (j - ny/2));
+				analytical_value(r, elapsed, D, bc, &cal);
+
+				/* shortest distance to right-wall source */
+				r = (j >= ny/2) ? dx * (nx-2 - i) : sqrt(dx*dx * (nx-2 - i)*(nx-2 - i) + dy*dy * (ny/2 - j)*(ny/2 - j));
+				analytical_value(r, elapsed, D, bc, &car);
+
+				/* superposition of analytical solutions */
+				ca = cal + car;
+
+				/* residual sum of squares (RSS) */
+				trss = (ca - cn) * (ca - cn) / (double)((nx-2) * (ny-2));
+				sum += trss;
+			}
+		}
+	}
+
+	*rss = sum;
+}

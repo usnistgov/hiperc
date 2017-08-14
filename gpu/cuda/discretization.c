7e1fde35a83499967f66519535957995516fff46
Trevor Keller <trevor.keller@gmail.com>
2017-08-13 20:58:46 -0400

    add OpenACC GPU diffusion code
    Closes #1, addresses #2

--- gpu/cuda/discretization.c
+++ gpu/cuda/discretization.c
@@ -21,11 +21,11 @@ void set_mask(double dx, double dy, int* nm, double** M)
 	/* M is initialized to zero, so corners can be ignored */
 	*nm = 1;
 
-	M[0][1] =  1.0 / (dy * dy); /* up */
-	M[1][0] =  1.0 / (dx * dx); /* left */
-	M[1][1] = -2.0 * (dx*dx + dy*dy) / (dx*dx * dy*dy); /* middle */
-	M[1][2] =  1.0 / (dx * dx); /* right */
-	M[2][1] =  1.0 / (dy * dy); /* down */
+	M[0][1] =  1. / (dy * dy); /* up */
+	M[1][0] =  1. / (dx * dx); /* left */
+	M[1][1] = -2. * (dx*dx + dy*dy) / (dx*dx * dy*dy); /* middle */
+	M[1][2] =  1. / (dx * dx); /* right */
+	M[2][1] =  1. / (dy * dy); /* down */
 }
 
 void compute_convolution(double** A, double** C, double** M, int nx, int ny, int nm)
@@ -38,7 +38,7 @@ void compute_convolution(double** A, double** C, double** M, int nx, int ny, int
 		#pragma omp for collapse(2)
 		for (j = 1; j < ny-1; j++) {
 			for (i = 1; i < nx-1; i++) {
-				value = 0.0;
+				value = 0.;
 				for (mj = -nm; mj < nm+1; mj++) {
 					for (mi = -nm; mi < nm+1; mi++) {
 						value += M[mj+nm][mi+nm] * A[j+mj][i+mi];
@@ -65,18 +65,14 @@ void step_in_time(double** A, double** B, double** C, int nx, int ny, double D, 
 	*elapsed += dt;
 }
 
-void analytical_value(double x, double t, double D, double bc[2][2], double* c)
-{
-	*c = bc[1][0] * (1.0 - erf(x / sqrt(4.0 * D * t)));
-}
-
 void check_solution(double** A, int nx, int ny, double dx, double dy, double elapsed, double D, double bc[2][2], double* rss)
 {
+	/* OpenCL does not have a GPU-based erf() definition, using Maclaurin series approximation */
 	double sum=0.;
 	#pragma omp parallel reduction(+:sum)
 	{
 		int i, j;
-		double r, cal, car, ca, cn, trss;
+		double ca, cal, car, cn, r, trss, z, z2;
 
 		#pragma omp for collapse(2)
 		for (j = 1; j < ny-1; j++) {
@@ -86,11 +82,15 @@ void check_solution(double** A, int nx, int ny, double dx, double dy, double ela
 
 				/* shortest distance to left-wall source */
 				r = (j < ny/2) ? dx * (i - 1) : sqrt(dx*dx * (i - 1) * (i - 1) + dy*dy * (j - ny/2) * (j - ny/2));
-				analytical_value(r, elapsed, D, bc, &cal);
+				z2 = z * z;
+				poly_erf = (z < 1.) ? 2. * z * (1. + z2 * (-1./3 + z2 * (1./10 + z2 * (-1./42 + z2 / 216)))) / M_PI : 1.;
+				cal = bc[1][0] * (1. - poly_erf);
 
 				/* shortest distance to right-wall source */
 				r = (j >= ny/2) ? dx * (nx-2 - i) : sqrt(dx*dx * (nx-2 - i)*(nx-2 - i) + dy*dy * (ny/2 - j)*(ny/2 - j));
-				analytical_value(r, elapsed, D, bc, &car);
+				z2 = z * z;
+				poly_erf = (z < 1.) ? 2. * z * (1. + z2 * (-1./3 + z2 * (1./10 + z2 * (-1./42 + z2 / 216)))) / M_PI : 1.;
+				car = bc[1][0] * (1. - poly_erf);
 
 				/* superposition of analytical solutions */
 				ca = cal + car;

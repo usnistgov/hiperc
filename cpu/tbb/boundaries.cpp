936db6b95974327bed6599d14fac07a12a4aea56
Trevor Keller <trevor.keller@gmail.com>
2017-08-14 11:56:51 -0400

    lambda functions in TBB (except parallel_reduce)
    Closes #25

--- cpu/tbb/boundaries.cpp
+++ cpu/tbb/boundaries.cpp
@@ -24,95 +24,65 @@ void set_boundaries(double bc[2][2])
 	bc[1][1] = chi; /* right boundary */
 }
 
-class ApplyInitialConditions2D {
-	double** my_A;
-	double my_c;
-
-	public:
-		/* constructor */
-		ApplyInitialConditions2D(double** A, double c) : my_A(A), my_c(c) {}
-
-		/* modifier */
-		void operator()(const tbb::blocked_range2d<int>& r) const
-		{
-			double** A = my_A;
-			double c = my_c;
-
-			for (int j = r.cols().begin(); j != r.cols().end(); j++)
-				for (int i = r.rows().begin(); i != r.rows().end(); i++)
-					A[j][i] = c;
-		}
-};
-
-class ApplyLeftBoundaryValues {
-	double** my_A;
-	double my_c;
-	int my_x;
-
-	public:
-		/* constructor */
-		ApplyLeftBoundaryValues(double** A, double c, int x) : my_A(A), my_c(c), my_x(x) {}
-
-		/* modifier */
-		void operator()(const tbb::blocked_range<int>& r) const
-		{
-			double** A = my_A;
-			double c = my_c;
-			int x = my_x;
-
-			for (int j = r.begin(); j != r.end(); j++)
-				A[j][x] = c;
-		}
-};
-
-class ApplyRightBoundaryValues {
-	double** my_A;
-	double my_c;
-	double my_x;
-
-	public:
-		/* constructor */
-		ApplyRightBoundaryValues(double** A, double c, int x) : my_A(A), my_c(c), my_x(x) {}
-
-		/* modifier */
-		void operator()(const tbb::blocked_range<int>& r) const
-		{
-			double** A = my_A;
-			double c = my_c;
-			int x = my_x;
-
-			for (int j = r.begin(); j != r.end(); j++)
-				A[j][x] = c;
-		}
-};
-
 void apply_initial_conditions(double** A, int nx, int ny, double bc[2][2])
 {
 	const int tbb_bs = 16;
 
-	tbb::parallel_for( tbb::blocked_range2d<int>(1, ny, tbb_bs, 1, nx, tbb_bs), ApplyInitialConditions2D(A, bc[0][0]) );
+	/* apply flat field values  (lambda function) */
+	tbb::parallel_for(tbb::blocked_range2d<int>(1, ny, tbb_bs, 1, nx, tbb_bs),
+		[=](const tbb::blocked_range2d<int>& r) {
+			for (int j = r.cols().begin(); j != r.cols().end(); j++) {
+				for (int i = r.rows().begin(); i != r.rows().end(); i++) {
+					A[j][i] = bc[0][0];
+				}
+			}
+		}
+	);
 
-	tbb::parallel_for( tbb::blocked_range<int>(1, ny/2, tbb_bs), ApplyLeftBoundaryValues(A, bc[1][0], 1) );
+	/* apply left boundary values  (lambda function) */
+	tbb::parallel_for(tbb::blocked_range<int>(1, ny/2, tbb_bs),
+		[=](const tbb::blocked_range<int>& r) {
+			for (int j = r.begin(); j != r.end(); j++) {
+				A[j][1] = bc[1][0];
+			}
+		}
+	);
 
-	tbb::parallel_for( tbb::blocked_range<int>(ny/2, ny-1, tbb_bs), ApplyRightBoundaryValues(A, bc[1][1], nx-2) );
+	/* apply right boundary values  (lambda function) */
+	tbb::parallel_for( tbb::blocked_range<int>(ny/2, ny-1, tbb_bs),
+		[=](const tbb::blocked_range<int>& r) {
+			for (int j = r.begin(); j != r.end(); j++) {
+				A[j][nx-2] = bc[1][1];
+			}
+		}
+	);
 }
 
-class ApplyBoundaryConditions {
-	double** my_A;
-	int my_nx;
-	int my_ny;
+void apply_boundary_conditions(double** A, int nx, int ny, double bc[2][2])
+{
+	const int tbb_bs = 16;
 
-	public:
-		/* constructor */
-		ApplyBoundaryConditions(double** A, int nx, int ny) : my_A(A), my_nx(nx), my_ny(ny) {}
+	/* apply left boundary values  (lambda function) */
+	tbb::parallel_for(tbb::blocked_range<int>(1, ny/2, tbb_bs),
+		[=](const tbb::blocked_range<int>& r) {
+			for (int j = r.begin(); j != r.end(); j++) {
+				A[j][1] = bc[1][0];
+			}
+		}
+	);
 
-		/* modifier */
-		void operator()(const tbb::blocked_range2d<int>& r) const
-		{
-			double** A = my_A;
-			int nx = my_nx;
-			int ny = my_ny;
+	/* apply right boundary values  (lambda function) */
+	tbb::parallel_for( tbb::blocked_range<int>(ny/2, ny-1, tbb_bs),
+		[=](const tbb::blocked_range<int>& r) {
+			for (int j = r.begin(); j != r.end(); j++) {
+				A[j][nx-2] = bc[1][1];
+			}
+		}
+	);
 
+	/* apply no-flux boundary conditions  (lambda function) */
+	tbb::parallel_for(tbb::blocked_range2d<int>(1, ny-1, tbb_bs, 1, nx-1, tbb_bs),
+		[=](const tbb::blocked_range2d<int>& r) {
 			for (int j = r.cols().begin(); j != r.cols().end(); j++) {
 				A[j][0] = A[j][1]; /* left boundary */
 				A[j][nx-1] = A[j][nx-2]; /* right boundary */
@@ -122,17 +92,6 @@ class ApplyBoundaryConditions {
 				A[0][i] = A[1][i]; /* bottom boundary */
 				A[ny-1][i] = A[ny-2][i]; /* top boundary */
 			}
-
 		}
-};
-
-void apply_boundary_conditions(double** A, int nx, int ny, double bc[2][2])
-{
-	const int tbb_bs = 16;
-
-	tbb::parallel_for( tbb::blocked_range<int>(1, ny/2, tbb_bs), ApplyLeftBoundaryValues(A, bc[1][0], 1) );
-
-	tbb::parallel_for( tbb::blocked_range<int>(ny/2, ny-1, tbb_bs), ApplyRightBoundaryValues(A, bc[1][1], nx-2) );
-
-	tbb::parallel_for( tbb::blocked_range2d<int>(1, ny-1, tbb_bs, 1, nx-1, tbb_bs), ApplyBoundaryConditions(A, nx, ny) );
+	);
 }

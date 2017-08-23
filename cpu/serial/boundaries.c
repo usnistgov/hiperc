8ad2b37be94a18104f51f03fefd9ccdf37ce0841
Trevor Keller <trevor.keller@gmail.com>
2017-08-23 01:04:04 -0400

    properly handle arbitrary mask sizes (serial CPU only)
    - Implemented 9-point stencil using 1st- and 3rd-nearest neighbors.
      Its 4x4 matrix form incurs more computational expensive than the
      canonical version, but provides a useful test case.
    - Adjusted boundary conditions to appropriately apply no-flux when
      more than one ghost cell is required.
    - Reverted example code to use 5-point method

--- cpu/serial/boundaries.c
+++ cpu/serial/boundaries.c
@@ -24,15 +24,17 @@ void apply_initial_conditions(fp_t** conc, int nx, int ny, int nm, fp_t bc[2][2]
 {
 	int i, j;
 
-	for (j = nm/2; j < ny; j++)
-		for (i = nm/2; i < nx; i++)
+	for (j = 0; j < ny; j++)
+		for (i = 0; i < nx; i++)
 			conc[j][i] = bc[0][0];
 
-	for (j = nm/2; j < ny/2; j++)
-		conc[j][nm/2] = bc[1][0]; /* left half-wall */
+	for (j = 0; j < ny/2; j++)
+		for (i = 0; i < 1+nm/2; i++)
+			conc[j][i] = bc[1][0]; /* left half-wall */
 
-	for (j = ny/2; j < ny-nm/2; j++)
-		conc[j][nx-nm/2-1] = bc[1][1]; /* right half-wall */
+	for (j = ny/2; j < ny; j++)
+		for (i = nx-1-nm/2; i < nx; i++)
+			conc[j][i] = bc[1][1]; /* right half-wall */
 }
 
 void apply_boundary_conditions(fp_t** conc, int nx, int ny, int nm, fp_t bc[2][2])
@@ -40,19 +42,25 @@ void apply_boundary_conditions(fp_t** conc, int nx, int ny, int nm, fp_t bc[2][2
 	/* Set fixed value (c=1) along left and bottom, zero-flux elsewhere */
 	int i, j;
 
-	for (j = nm/2; j < ny/2; j++)
-		conc[j][nm/2] = bc[1][0]; /* left value */
+	for (j = 0; j < ny/2; j++)
+		for (i = 0; i < 1+nm/2; i++)
+			conc[j][i] = bc[1][0]; /* left value */
 
-	for (j = ny/2; j < ny-nm/2; j++)
-		conc[j][nx-nm/2-1] = bc[1][1]; /* right value */
+	for (j = ny/2; j < ny; j++)
+		for (i = nx-1-nm/2; i < nx; i++)
+			conc[j][i] = bc[1][1]; /* right value */
 
-	for (j = nm/2; j < ny-nm/2; j++) {
-		conc[j][nm/2-1] = conc[j][nm/2]; /* left condition */
-		conc[j][nx-nm/2] = conc[j][nx-nm/2-1]; /* right condition */
+	for (j = 0; j < ny; j++) {
+		for (i = nm/2; i > 0; i--)
+			conc[j][i-1] = conc[j][i]; /* left condition */
+		for (i = nx-1-nm/2; i < nx-1; i++)
+			conc[j][i+1] = conc[j][i]; /* right condition */
 	}
 
-	for (i = nm/2; i < nx-nm/2; i++) {
-		conc[nm/2-1][i] = conc[nm/2][i]; /* bottom condition */
-		conc[ny-nm/2][i] = conc[ny-nm/2-1][i]; /* top condition */
+	for (i = 0; i < nx; i++) {
+		for (j = nm/2; j > 0; j--)
+			conc[j-1][i] = conc[j][i]; /* bottom condition */
+		for (j = ny-1-nm/2; j < ny-1; j++)
+			conc[j+1][i] = conc[j][i]; /* top condition */
 	}
 }

5b012e5e9035925bd2512e9dc3c75376c1d6ab26
Trevor Keller <trevor.keller@gmail.com>
2017-08-23 22:02:27 -0400

    insert terms of use in source code

--- cpu/tbb/boundaries.cpp
+++ cpu/tbb/boundaries.cpp
@@ -1,5 +1,22 @@
+/**********************************************************************************
+ This file is part of Phase-field Accelerator Benchmarks, written by Trevor Keller
+ and available from https://github.com/usnistgov/phasefield-accelerator-benchmarks.
+
+ This software was developed at the National Institute of Standards and Technology
+ by employees of the Federal Government in the course of their official duties.
+ Pursuant to title 17 section 105 of the United States Code this software is not
+ subject to copyright protection and is in the public domain. NIST assumes no
+ responsibility whatsoever for the use of this software by other parties, and makes
+ no guarantees, expressed or implied, about its quality, reliability, or any other
+ characteristic. We would appreciate acknowledgement if the software is used.
+
+ This software can be redistributed and/or modified freely provided that any
+ derivative works bear some notice that they are derived from it, and any modified
+ versions bear some notice that they have been modified.
+ **********************************************************************************/
+
 /*
-	File: boundaries.c
+	File: boundaries.cpp
 	Role: implementation of boundary condition functions with TBB threading
 
 	Questions/comments to trevor.keller@nist.gov

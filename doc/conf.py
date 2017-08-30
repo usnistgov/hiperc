a0f51fa91b1ccd931c6cde9b4c6cdd58414d924e
Trevor Keller <trevor.keller@nist.gov>
2017-08-30 11:53:33 -0400

    forgotten import

--- doc/conf.py
+++ doc/conf.py
@@ -17,7 +17,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
-# import sys
+import subprocess
 # sys.path.insert(0, os.path.abspath('.'))
 
 # readthedocs requires this tag

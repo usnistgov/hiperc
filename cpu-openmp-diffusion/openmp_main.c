eac528ed81369eabccde426e4e74d3a6f0576ecd
Trevor Keller <trevor.keller@gmail.com>
2017-11-16 16:36:19 -0500

    Timestep in main and simplify BCs, for CPU diffusion problems
    Addresses #122, addresses #126

--- cpu-openmp-diffusion/openmp_main.c
+++ cpu-openmp-diffusion/openmp_main.c
@@ -28,7 +28,6 @@
 #include <string.h>
 
 #include "boundaries.h"
-#include "discretization.h"
 #include "mesh.h"
 #include "numerics.h"
 #include "output.h"
@@ -45,13 +44,12 @@ int main(int argc, char* argv[])
 	fp_t **conc_old, **conc_new, **conc_lap, **mask_lap;
 	int bx=32, by=32, nx=512, ny=512, nm=3, code=53;
 	fp_t dx=0.5, dy=0.5, h;
-	fp_t bc[2][2];
 
 	/* declare default materials and numerical parameters */
 	fp_t D=0.00625, linStab=0.1, dt=1., elapsed=0., rss=0.;
 	int step=0, steps=100000, checks=10000;
 	double start_time=0.;
-	struct Stopwatch sw = {0., 0., 0., 0.};
+	struct Stopwatch watch = {0., 0., 0., 0.};
 
 	StartTimer();
 
@@ -63,11 +61,12 @@ int main(int argc, char* argv[])
 	/* initialize memory */
 	make_arrays(&conc_old, &conc_new, &conc_lap, &mask_lap, nx, ny, nm);
 	set_mask(dx, dy, code, mask_lap, nm);
-	set_boundaries(bc);
+
+	print_progress(0, steps);
 
 	start_time = GetTimer();
-	apply_initial_conditions(conc_old, nx, ny, nm, bc);
-	sw.step = GetTimer() - start_time;
+	apply_initial_conditions(conc_old, nx, ny, nm);
+	watch.step = GetTimer() - start_time;
 
 	/* write initial condition data */
 	start_time = GetTimer();
@@ -79,46 +78,45 @@ int main(int argc, char* argv[])
 		printf("Error: unable to %s for output. Check permissions.\n", "runlog.csv");
 		exit(-1);
 	}
-	sw.file = GetTimer() - start_time;
+	watch.file = GetTimer() - start_time;
 
 	fprintf(output, "iter,sim_time,wrss,conv_time,step_time,IO_time,soln_time,run_time\n");
-	fprintf(output, "%i,%f,%f,%f,%f,%f,%f,%f\n", step, elapsed, rss, sw.conv, sw.step, sw.file, sw.soln, GetTimer());
+	fprintf(output, "%i,%f,%f,%f,%f,%f,%f,%f\n", step, elapsed, rss,
+			watch.conv, watch.step, watch.file, watch.soln, GetTimer());
 	fflush(output);
 
-	/* Note: block is equivalent to a typical
-	  for (int step=1; step < steps+1; step++),
-	  1-indexed so as not to overwrite the initial condition image,
-	  but the loop-internals are farmed out to a coprocessor.
-	  So we use a while loop instead. */
-
 	/* do the work */
-	step = 0;
-	print_progress(step, steps);
-	while (step < steps) {
-		if (checks > steps - step)
-			checks = steps - step;
+	for (step = 1; step < steps+1; step++) {
+		print_progress(step, steps);
 
-		assert(step + checks <= steps);
+		/* === Start Architecture-Specific Kernel === */
+		apply_boundary_conditions(conc_old, nx, ny, nm);
 
-		solve_diffusion_equation(conc_old, conc_new, conc_lap, mask_lap, nx, ny,
-		                         nm, bc, D, dt, checks, &elapsed, &sw);
-		/* returns after swapping pointers: new data is in conc_old */
+		start_time = GetTimer();
+		compute_convolution(conc_old, conc_lap, mask_lap, nx, ny, nm);
+		watch.conv += GetTimer() - start_time;
 
-		for (int i = 0; i < checks; i++) {
-			step++;
-			print_progress(step, steps);
+		start_time = GetTimer();
+		update_composition(conc_old, conc_lap, conc_new, nx, ny, nm, D, dt);
+		watch.step += GetTimer() - start_time;
+
+		swap_pointers(&conc_old, &conc_new);
+		elapsed += dt;
+		/* === Finish Architecture-Specific Kernel === */
+
+		if (step % checks == 0) {
+			start_time = GetTimer();
+			write_png(conc_old, nx, ny, step);
+			watch.file += GetTimer() - start_time;
+
+			start_time = GetTimer();
+			check_solution(conc_old, conc_lap, nx, ny, dx, dy, nm, elapsed, D, &rss);
+			watch.soln += GetTimer() - start_time;
+
+			fprintf(output, "%i,%f,%f,%f,%f,%f,%f,%f\n", step, elapsed, rss,
+					watch.conv, watch.step, watch.file, watch.soln, GetTimer());
+			fflush(output);
 		}
-
-		start_time = GetTimer();
-		write_png(conc_old, nx, ny, step);
-		sw.file += GetTimer() - start_time;
-
-		start_time = GetTimer();
-		check_solution(conc_old, conc_lap, nx, ny, dx, dy, nm, elapsed, D, bc, &rss);
-		sw.soln += GetTimer() - start_time;
-
-		fprintf(output, "%i,%f,%f,%f,%f,%f,%f,%f\n", step, elapsed, rss, sw.conv, sw.step, sw.file, sw.soln, GetTimer());
-		fflush(output);
 	}
 
 	write_csv(conc_old, nx, ny, dx, dy, step);
